36a99b72071824a1e109a118810bb35b15ec0cb7
isabella montoya serna <isams2004.1@gmail.com>
2023-08-02 09:12:23 -0500

    probamos cambiando de 500 a 1000

--- ARDUINO/ARDUINO.ino
+++ ARDUINO/ARDUINO.ino
@@ -1,14 +1,17 @@
-void setup() {
+void setup() 
+{
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
+void loop() 
+{
   static uint32_t previousTime = 0;
   static bool ledState = true;
 
   uint32_t currentTime = millis();
 
-  if( (currentTime - previousTime) > 1000){
+  if( (currentTime - previousTime) > 1000)
+  {
     previousTime = currentTime;
     ledState = !ledState;
     digitalWrite(LED_BUILTIN, ledState);
